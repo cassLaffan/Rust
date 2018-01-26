c47840f9ae51c43581442a91c206d5c20188b133
Cassandra <poisonberryvines@gmail.com>
2018-01-26 11:10:29 -0500

    Adding more tic tac toe functionality

--- x_and_o/src/main.rs
+++ x_and_o/src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+use std::cmp::Ordering;
+
 fn main() {
     //and attempt at a simple game of xs and os
     //learning goal: try and make a turn based game
@@ -7,5 +10,36 @@ fn main() {
     println!("_|_|_");
     println!("_|_|_");
 
-    println!("");
+    loop{
+        println!("Player one, please choose a row from 1 to 3 and then a column from 1 to 3.");
+
+        println!("Row: ");
+        let mut rowOne = String::new();
+        io::stdin().read_line(&mut rowOne).expect("Failed to read input.");
+
+        println!("Column: ");
+        let mut colOne = String::new();
+        io::stdin().read_line(&mut colOne).expect("Failed to read input.");
+
+        if(){
+            println!("Player one wins!");
+            break;
+        }
+
+        println!("Player two, please choose a row from 1 to 3 and then a column from 1 to 3.");
+
+        println!("Row: ");
+        let mut rowTwo = String::new();
+        io::stdin().read_line(&mut rowTwo).expect("Failed to read input.");
+
+        println!("Column: ");
+        let mut colTwo = String::new();
+        io::stdin().read_line(&mut colTwo).expect("Failed to read input.");
+
+        if(){
+            println!("Player one wins!");
+            break;
+        }
+    }
+
 }
