4b42f026eef80b0da7c364c2b03b1edc51de8994
Cassandra <poisonberryvines@gmail.com>
2018-01-26 10:59:50 -0500

    Adding a game of Tic Tac Toe

---
+++ x_and_o/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    //and attempt at a simple game of xs and os
+    //learning goal: try and make a turn based game
+    println!("Tic Tac Toe!");
+    println!("Rules: Both players must attempt to put three of their assigned symbol in a straight line. X goes first.");
+    println!("_|_|_");
+    println!("_|_|_");
+    println!("_|_|_");
+
+    println!("");
+}
---
+++ x_and_o/target/debug/x_and_o.d
@@ -0,0 +1,1 @@
+/home/cassandra/Documents/Rust/x_and_o/target/debug/x_and_o: /home/cassandra/Documents/Rust/x_and_o/src/main.rs
