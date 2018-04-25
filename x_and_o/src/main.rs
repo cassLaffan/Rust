310ff9d634f5218a6f85cd03d0d93a9d8356cd54
Cassandra <poisonberryvines@gmail.com>
2018-04-25 10:33:10 -0400

    Trying to fix bug that doesn't allow parsing to int

--- x_and_o/src/main.rs
+++ x_and_o/src/main.rs
@@ -2,44 +2,46 @@ use std::io;
 use std::cmp::Ordering;
 
 //this function will loop so long as a player's input is not valid
-fn turn_loop_row() -> i32{
-    let mut rowChoice: i32;
+fn turn_loop_row(reader: stdin) -> i32{
+    let mut row_choice: i32;
     loop{
         println!("Row: ");
         let mut row = String::new();
-        io::stdin().read_line(&mut row).expect("Failed to read input.");
-        rowChoice = row.parse().unwrap_or_default();
+        reader.read_line(&mut row).expect("Failed to read input.");
+        row_choice = row.trim().parse::<i32>();
 
-        if(rowChoice < 1 || rowChoice > 3){
+        if(row_choice < 1 || row_choice > 3){
             println!("Please choose a row between 1 and 3.");
+            println!("{}", row_choice);
         }
         else{
             break;
         }
     }
-    return rowChoice;
+    return row_choice;
 }
 
 //this function will loop so long as a player's input is not valid
-fn turn_loop_column() -> i32{
-    let mut colChoice: i32;
+fn turn_loop_column(reader: stdin) -> i32{
+    let mut col_choice: i32;
     loop{
         println!("Column: ");
         let mut col = String::new();
-        io::stdin().read_line(&mut col).expect("Failed to read input.");
-        colChoice = col.parse().unwrap_or_default();
+        reader.read_line(&mut col).expect("Failed to read input.");
+        col_choice = col.parse().unwrap_or_default();
 
-        if(colChoice < 1 || colChoice > 3){
+        if(col_choice < 1 || col_choice > 3){
             println!("Please choose a column between 1 and 3.");
         }
         else{
             break;
         }
     }
-    return colChoice;
+    return col_choice;
 }
 
 fn main() {
+    let reader = io::stdin();
     //and attempt at a simple game of xs and os
     //learning goal: try and make a turn based game
     println!("Tic Tac Toe!");
@@ -48,37 +50,37 @@ fn main() {
     println!("_|_|_");
     println!("_|_|_");
 
-    let mut rowOneArray : [i32; 3] = [0, 0, 0];
-    let mut rowTwoArray : [i32; 3] = [0, 0, 0];
-    let mut rowThreeArray : [i32; 3] = [0, 0, 0];//keeps track of whats happening
-    let mut winState = false;
+    let mut row_one_array : [i32; 3] = [0, 0, 0];
+    let mut row_two_rray : [i32; 3] = [0, 0, 0];
+    let mut row_three_array : [i32; 3] = [0, 0, 0];//keeps track of whats happening
+    let mut win_state = false;
 
     loop{
-        let mut colChoiceOne: i32;
-        let mut rowChoiceOne: i32;
+        let mut col_choice_one: i32;
+        let mut row_choice_one: i32;
 
         println!("Player one, please choose a row from 1 to 3.");
-        rowChoiceOne = turn_loop_row();
+        row_choice_one = turn_loop_row(reader);
 
         println!("Player one, please choose a column from 1 to 3.");
-        colChoiceOne = turn_loop_column();
+        col_choice_one = turn_loop_column(reader);
 
         //will loop until the player enters a correct input
 
-        if(winState){
+        if(win_state){
             println!("Player one wins!");
             break;
         }
-        let mut colChoiceTwo: i32;
-        let mut rowChoiceTwo: i32;
+        let mut col_choice_two: i32;
+        let mut row_choice_two: i32;
 
         println!("Player two, please choose a row from 1 to 3.");
-        rowChoiceTwo = turn_loop_row();
+        row_choice_two = turn_loop_row(reader);
 
         println!("Player two, please choose a column from 1 to 3.");
-        colChoiceTwo = turn_loop_column();
+        col_choice_two = turn_loop_column(reader);
 
-        if(winState){
+        if(win_state){
             println!("Player one wins!");
             break;
         }
